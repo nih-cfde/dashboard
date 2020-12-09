aba9a83ae1853d0cad2a0b3a8e23940783964cee
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-12-08 21:05:07 -0500

    Add separate HTML page that contains a single summary chart. Closes #24.

---
+++ js/summary_graph.js
@@ -0,0 +1,70 @@
+/* global register_dropdowns update_chart */
+
+// TODO - copied from dcc_review.js
+function get_formatted_date(d) {
+    // Returns a date as a string in the following format: "2020-10-09 17:32:32 - 0400"
+    var month = d.getMonth() + 1;
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    var day = d.getDate();
+
+    if (day < 10) {
+        day = '0' + day;
+    }
+
+    var formatted = d.getFullYear() + '-' + month + '-' + day
+            + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds()
+            + ' ' + get_time_zone_diff(d);
+
+    return formatted;
+}
+
+// END copied from dcc_review.js
+
+function get_time_zone_diff(d) {
+    var timezone_offset_minutes = d.getTimezoneOffset();
+    var offset_hrs = parseInt(Math.abs(timezone_offset_minutes / 60));
+    var offset_min = Math.abs(timezone_offset_minutes % 60);
+
+    if (offset_hrs < 10) {
+        offset_hrs = '0' + offset_hrs;
+    }
+
+    if (offset_min < 10) {
+        offset_min = '0' + offset_min;
+    }
+
+    // Add an opposite sign to the offset
+    // If offset is 0, it means timezone is UTC
+    var timezone_standard;
+
+    if (timezone_offset_minutes < 0)
+        timezone_standard = '+ ' + offset_hrs  + offset_min;
+    else if (timezone_offset_minutes > 0)
+        timezone_standard = '- ' + offset_hrs  + offset_min;
+    else if (timezone_offset_minutes == 0)
+        timezone_standard = 'Z ';
+    return timezone_standard;
+}
+
+$(document).ready(function() {
+
+    var CATALOG_ID = 1;
+
+    // chart 1 - stacked bar graph
+    register_dropdowns('sbc1');
+    update_chart('sbc1');
+
+    // TODO - set these individually based on API response
+    // update last updated
+    var summary_data_url = './data/summary.json';
+    $.getJSON(summary_data_url, function(data) {
+	var d = new Date(data['last_updated']);
+        var formatted_date = get_formatted_date(d);
+        $('#sbc1-last_updated').append('Last updated: ' + formatted_date);
+    });
+
+});
