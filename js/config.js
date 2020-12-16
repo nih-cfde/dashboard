723840a639d2e877798f8f8a51093ed394faf6f6
Jonathan Crabtree <jonathancrabtree@gmail.com>
2020-12-16 18:47:45 -0500

    Add graph group/category limits to config file.

--- js/config.js
+++ js/config.js
@@ -4,6 +4,9 @@
 const DASHBOARD_API_URL='http://localhost:5000';
 const DERIVA_URL='http://app-dev.nih-cfde.org';
 
+const MAX_GRAPH_GROUP1 = 12;
+const MAX_GRAPH_GROUP2 = 5;
+
 // read DERIVA catalog id from URL
 function get_catalog_id() {
     var url = new URL(window.location.href);
