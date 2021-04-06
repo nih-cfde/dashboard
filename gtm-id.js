bae0d9cd164425c80494db2b0d03cb1e60aa9493
Joshua Chudy <jchudy@isi.edu>
2021-04-06 15:13:55 -0700

    update gtm-id.js to use gtag.js instead of gtm.js

--- gtm-id.js
+++ gtm-id.js
@@ -10,7 +10,7 @@ var gtmId = 'G-LNZN3CXFQK'; // Change null to be your GTM container ID (e.g. "GT
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    'https://www.googletagmanager.com/gtag/js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer', gtmId);
 
     // Insert <body> tag
