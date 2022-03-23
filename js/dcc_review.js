faafb0571d28ba5d0d7fc40f9dcb8d50f5b64fec
mschor <mschor@ip-192-168-1-226.ec2.internal>
2022-03-23 12:23:40 -0400

    Updates for FAIR metrics display

--- js/dcc_review.js
+++ js/dcc_review.js
@@ -4,8 +4,14 @@ const UPDATE_DELAY_SECS = 0.05;
 var update_pending = false;
 var last_update_time = null;
 
+// FAIR vars
+var metrics_chart_width = 0;
+var metrics_chart_height = 0;
+var fair_metrics_left_svg;
+var fair_metrics_right_svg;
+
+
 function window_resized(catalog_id, chart_id) {
-    console.log("In window_resized");
     last_update_time = new Date().getTime();
     const utime = last_update_time;
     setTimeout(function() {
@@ -153,71 +159,73 @@ function get_chaise_uri(catalog_id, entity, DCC_NID="") {
 }
 
 function add_summary_data(catalog_id, DCC, num_dccs) {
-    var dcc_summary_url = DASHBOARD_API_URL + '/dcc/' + DCC['id'];
-    if (catalog_id != null) dcc_summary_url += '?catalogId=' + catalog_id;
+  var dcc_summary_url = DASHBOARD_API_URL + '/dcc/' + DCC['id'];
+  if (catalog_id != null) dcc_summary_url += '?catalogId=' + catalog_id;
 
-    // counts for top-level entities, except project
-    get_json_retry(dcc_summary_url, function(data) {
-        Object.keys(data).forEach(function(key) {
-            if (key.endsWith('_count') && (!key.startsWith('toplevel_project'))) {
-                var entity =  key.slice(0, key.length - '_count'.length);
-                var name = 'Total ' + key.charAt(0).toUpperCase() + key.slice(1, key.length - '_count'.length) + 's';
-                // database was changed to prefer using/sending "nid" instead of "RID" since RID will change from catalog to catalog
-                var chaise_uri = get_chaise_uri(catalog_id, entity, data['nid']);
-		$('#' + key).html('<a href="' + chaise_uri + '">' + data[key].toLocaleString() + '</a>');
-	    }
-	});
-
-        $('#dcc_name').append(data['complete_name']);
-	var data_url = DERIVA_URL + '/chaise/recordset/#registry/CFDE:datapackage';
-	if (data['datapackage_RID'] != null) data_url += '/RID=' + data['datapackage_RID'];
-
-        $('#datapackage_link').prop('href', data_url);
-        $('#datapackage_link').prepend(data_url);
-	$('#data_review_title')[0].innerHTML = data['abbreviation'] +  ' Data Review';
-
-	if (num_dccs == 1) {
-            $('#data_snapshot_title')[0].innerHTML = data['abbreviation'] +  ' Data Snapshot';
-	} else {
-            $('#data_snapshot_title')[0].innerHTML =  'Data Snapshot';
-	}
-        var d = new Date(data['last_updated']);
-        var formatted_date = get_formatted_date(d);
-        $('#last_updated').append('Last updated: ' + formatted_date);
+  // counts for top-level entities, except project
+  get_json_retry(dcc_summary_url, function (data) {
+    Object.keys(data).forEach(function (key) {
+      if (key.endsWith('_count') && (!key.startsWith('toplevel_project'))) {
+        var entity = key.slice(0, key.length - '_count'.length);
+        var name = 'Total ' + key.charAt(0).toUpperCase() + key.slice(1, key.length - '_count'.length) + 's';
+        // database was changed to prefer using/sending "nid" instead of "RID" since RID will change from catalog to catalog
+        var chaise_uri = get_chaise_uri(catalog_id, entity, data['nid']);
+        $('#' + key).html('<a href="' + chaise_uri + '">' + data[key].toLocaleString() + '</a>');
+      }
     });
 
-    // counts for top-level entities with links to other entities of a specified type (e.g., Subjects with File)
-    var dcc_linkcount_url = DASHBOARD_API_URL + '/dcc/' + DCC['id'] + '/linkcount';
-    if (catalog_id != null) dcc_linkcount_url += '?catalogId=' + catalog_id;
+    $('#dcc_name').append(data['complete_name']);
+    var data_url = DERIVA_URL + '/chaise/recordset/#registry/CFDE:datapackage';
+    if (data['datapackage_RID'] != null) {
+      data_url += '/RID=' + data['datapackage_RID'];
+    }
 
-    get_json_retry(dcc_linkcount_url, function(data) {
-        // iterate over keys like "subject_count" and "subject_with_biosample_count"
-        // remove "_count", append an 's' at the end of the first word, make
-        // first and last words title case, and replace "_" with " "
-        Object.keys(data).forEach(function(key) {
-	    if (key.endsWith('_count') && (!key.startsWith('toplevel_project'))) {
-                var name = key.slice(0, key.length - '_count'.length);
-                name = name.charAt(0).toUpperCase() + name.slice(1);
-                var idx = name.indexOf('_');
+    $('#datapackage_link').prop('href', data_url);
+    $('#datapackage_link').prepend(data_url);
+    $('#data_review_title')[0].innerHTML = data['abbreviation'] + ' Data Review';
 
-                if (idx == -1) {
-                    name = name + 's';
-                } else {
-                    name = name.replace('_', 's_');
-                }
+    if (num_dccs == 1) {
+      $('#data_snapshot_title')[0].innerHTML = data['abbreviation'] + ' Data Snapshot';
+    } else {
+      $('#data_snapshot_title')[0].innerHTML = 'Data Snapshot';
+    }
+    var d = new Date(data['last_updated']);
+    var formatted_date = get_formatted_date(d);
+    $('#last_updated').append('Last updated: ' + formatted_date);
+  });
 
-                idx = name.lastIndexOf('_');
+  // counts for top-level entities with links to other entities of a specified type (e.g., Subjects with File)
+  var dcc_linkcount_url = DASHBOARD_API_URL + '/dcc/' + DCC['id'] + '/linkcount';
+  if (catalog_id != null) dcc_linkcount_url += '?catalogId=' + catalog_id;
 
-                if (idx > 0) {
-                    name = name.slice(0, idx + 1) + name.charAt(idx + 1).toUpperCase() + name.slice(idx + 2);
-                }
+  get_json_retry(dcc_linkcount_url, function (data) {
+    // iterate over keys like "subject_count" and "subject_with_biosample_count"
+    // remove "_count", append an 's' at the end of the first word, make
+    // first and last words title case, and replace "_" with " "
+    Object.keys(data).forEach(function (key) {
+      if (key.endsWith('_count') && (!key.startsWith('toplevel_project'))) {
+        var name = key.slice(0, key.length - '_count'.length);
+        name = name.charAt(0).toUpperCase() + name.slice(1);
+        var idx = name.indexOf('_');
 
-                var chaise_uri = get_chaise_uri(catalog_id, name.toLowerCase(), data['RID']);
-                name = name.replace(/_/g, ' ');
-               $('#data_breakdown_table >> #' + key).html('<a href="' + chaise_uri + '">' + data[key].toLocaleString() + '</a>');
-            }
-        });
+        if (idx == -1) {
+          name = name + 's';
+        } else {
+          name = name.replace('_', 's_');
+        }
+
+        idx = name.lastIndexOf('_');
+
+        if (idx > 0) {
+          name = name.slice(0, idx + 1) + name.charAt(idx + 1).toUpperCase() + name.slice(idx + 2);
+        }
+
+        var chaise_uri = get_chaise_uri(catalog_id, name.toLowerCase(), data['RID']);
+        name = name.replace(/_/g, ' ');
+        $('#data_breakdown_table >> #' + key).html('<a href="' + chaise_uri + '">' + data[key].toLocaleString() + '</a>');
+      }
     });
+  });
 }
 
 function loadScript(url) {
@@ -234,12 +242,193 @@ function loadScript(url) {
     document.getElementsByTagName('head')[0].appendChild(script);
 }
 
+function metrics_width(w) {
+  return w - 300;
+}
+
+function init_metrics_chart() {
+  // set the dimensions and margins of the graph
+  var margin = { top: 20, right: 30, bottom: 40, left: 0 };
+  metrics_chart_height = 400 - margin.top - margin.bottom;
+  metrics_chart_width = 570;
+
+  // append the svg object to the body of the page
+  fair_metrics_left_svg = d3.select("#fair_graph_left")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", metrics_chart_height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+  fair_metrics_right_svg = d3.select("#fair_graph_right")
+    .append("svg")
+    .attr("width", "100%")
+    .attr("height", metrics_chart_height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+}
+
+function update_metrics_chart(catalog_id, chart_id) {
+
+  var data_url = DASHBOARD_API_URL + '/fair/' + catalog_id;
+
+  if (!(chart_id in REQUESTNUMS)) {
+    REQUESTNUMS[chart_id] = 0;
+  }
+  const requestnum = ++REQUESTNUMS[chart_id];
+
+  // define the success and failure callbacks
+  var data_fn = function (data) {
+    // ignore out-of-sequence responses
+    if (requestnum == REQUESTNUMS[chart_id]) {
+      chart_data[chart_id] = data;
+      chart_data_urls[chart_id] = data_url;
+      draw_metrics_chart(chart_id, data);
+    }
+  };
+  var fail_fn = function (jqXHR, status, error) {
+    // ignore out-of-sequence responses
+    if (requestnum == REQUESTNUMS[chart_id]) {
+      console.error('Error loading data for chart.');
+      show_error(chart_id);
+    }
+  };
+
+  // get the data and invoke the appropriate callback
+  get_json_retry(data_url, data_fn, fail_fn);
+}
+
+
+function draw_metrics_chart(chart_id, data) {
+
+  // if no metrics returned
+  if (data.length == 0) {
+    // set contents of "fair_metrics_wrapper" to be something like: "No metrics for data submission."
+    $('#' + chart_id).html("<p>No FAIR metrics loaded for this submission.</p>");
+    return;
+  }
+
+  if (data.length % 2 == 0) {
+    data1 = data.slice(0, data.length / 2);
+    data2 = data.slice(data.length / 2);
+  } else {
+    data1 = data.slice(0, (data.length + 1) / 2);
+    data2 = data.slice((data.length + 1) / 2);
+  }
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 100])
+    .range([0, metrics_width(metrics_chart_width) ]);
+
+  fair_metrics_left_svg.append("g")
+    .attr("transform", "translate(0," + metrics_chart_height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+  fair_metrics_right_svg.append("g")
+    .attr("transform", "translate(0," + metrics_chart_height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+
+  // Y axis
+  var y1 = d3.scaleBand()
+    .range([0, metrics_chart_height])
+    .domain(data1.map(function (d) { return d.name;  }))
+    .padding(.1);
+  var y2 = d3.scaleBand()
+    .range([0, metrics_chart_height])
+    .domain(data2.map(function (d) { return d.name; }))
+    .padding(.1);
+  fair_metrics_left_svg.append("g")
+    .call(d3.axisLeft(y1))
+    .style("font-size", "1rem");
+  fair_metrics_right_svg.append("g")
+    .call(d3.axisLeft(y2))
+    .style("font-size", "1rem");
+
+  // truncate y axis labels and add ellipses 
+  fair_metrics_left_svg.selectAll('text').each(ellipsize(310,5));
+  fair_metrics_right_svg.selectAll('text').each(ellipsize(310,5));
+
+  var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden")
+    .style("background", "#FFF")
+    .style("border", "2px solid black")
+    .style("padding-left", "5px")
+    .style("padding-right", "5px")
+    .text("a simple tooltip");
+
+
+  fair_metrics_left_svg.selectAll()
+    .data(data1)
+    .enter()
+    .append("rect")
+    .attr("x", function (d) { return 0; })
+    .attr("y", function (d) { return y1(d.name); })
+    .attr("width", function (d) {
+      if (x(d.fair_count) == 0)
+        return 1;
+      return (metrics_width(metrics_chart_width)) * d.fair_count / d.total_count })
+    .attr("height", y1.bandwidth())
+    .attr("fill", "#8cc145")
+    .on("mouseover", function (d) { tooltip.text(d.fair_count + " / " + d.total_count); return tooltip.style("visibility", "visible"); })
+    .on("mousemove", function () { return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 15) + "px"); })
+    .on("mouseout", function () { return tooltip.style("visibility", "hidden"); });
+
+  var maxw = 0;
+
+  fair_metrics_left_svg.selectAll("text").each(function () {
+    if (this.getBBox().width > maxw) {
+      maxw = this.getBBox().width;
+    }
+  });
+  maxw += 10;
+  fair_metrics_left_svg.attr("transform", "translate(" + maxw + ",0)");
+  fair_metrics_left_svg.attr("preserveAspectRatio", "xMinYMin meet")
+  fair_metrics_left_svg.attr("viewBox", "0 0 960 500")
+
+  var maxw = 0;
+  fair_metrics_right_svg.selectAll("text").each(function () {
+    if (this.getBBox().width > maxw) {
+      maxw = this.getBBox().width;
+    }
+  });
+  maxw += 10;
+  fair_metrics_right_svg.attr("transform", "translate(" + maxw + ",0)");
+
+
+  fair_metrics_right_svg.selectAll()
+    .data(data2)
+    .enter()
+    .append("rect")
+    .attr("x", function (d) { return x(0) })
+    .attr("y", function (d) { return y2(d.name); })
+    .attr("width", function (d) { return (metrics_width(metrics_chart_width)) * d.fair_count / d.total_count })
+    .attr("height", y2.bandwidth())
+    .attr("fill", "#8cc145")
+    .on("mouseover", function (d) { tooltip.text(d.fair_count + " / " + d.total_count); return tooltip.style("visibility", "visible"); })
+    .on("mousemove", function () { return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"); })
+    .on("mouseout", function () { return tooltip.style("visibility", "hidden"); })
+};
+
+
 $(document).ready(function() {
+    init_metrics_chart();
     var catalog_id = get_catalog_id();
     var dcc = null;
     // the DCC review page assumes that the specified catalog contains data for a single DCC only
     var dcc_list_url = DASHBOARD_API_URL + '/dcc';
-    if (catalog_id != null) {
+    if (catalog_id != null && typeof chaiseConfig != 'undefined' ) { 
         chaiseConfig.defaultCatalog = catalog_id;
         dcc_list_url += '?catalogId=' + catalog_id;
     }
@@ -251,13 +440,16 @@ $(document).ready(function() {
 
     get_json_retry(dcc_list_url, function(data) {
         if (data.length != 1) {
-	    console.log('WARNING: DERIVA CATALOG_ID  ' + catalog_id + ' contains ' + ((data.length > 1) ? 'data from multiple DCsC' : 'no data'));
+	        console.log('WARNING: DERIVA CATALOG_ID  ' + catalog_id + ' contains ' + ((data.length > 1) ? 'data from multiple DCsC' : 'no data'));
         }
+  console.log("Data length is: " + data.length);
 	if (data.length > 0) {
 	    dcc = data[0];
 	    add_summary_data(catalog_id, dcc, data.length);
         }
     });
+
+    update_metrics_chart(catalog_id, 'fair_metrics_wrapper');
 
     window.addEventListener('resize', function() { window_resized(catalog_id, 'review_bc1'); });
 });
